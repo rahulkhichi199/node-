a35cc5dff7f58bb83566ec5b52444bd21b64da2d
rahulkhichi199 <rahulkhichi331@gmail.com>
2022-02-08 22:27:26 +0530

    done again

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+var server = app.listen(3000, () => {
+  console.log("Listening on port " + server.address().port + "...");
+});
