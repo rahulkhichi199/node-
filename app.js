0d78d3a626fa94ef6ed65c514fbcc6eb8256eaba
rahulkhichi199 <rahulkhichi331@gmail.com>
2022-03-31 12:51:46 +0530

    hello

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 
 //Define request response in root URL (/)
 app.get("/", function (req, res) {
-  res.send("Hello lokesh");
+  res.send("Hello world");
 });
 
 //Launch listening server on port 8080
